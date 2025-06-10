838a41e63453d18435baaa20af5a1412db8c0dbf
Glitch (glitch-hello-website) <none>
2025-06-10 18:46:06 +0000

    🏂🍫 Checkpoint ./services.py:71838919/28

--- services.py
+++ services.py
@@ -312,7 +312,7 @@ def administrar_chatbot(text, number, messageId, name):
     time.sleep(2)
 
     if text in ["hola", "buenos días", "buenas tardes", "volver al menú principal"]:
-        body = "¡Hola! 🤖 Bienvenido a este taller"
+        body = "¡Hola! 🤖 Bienvenido a este taller el dia de hoy 10 de julio"
         footer = "Equipo UNSIJ"
         replyButtonData = listReply_Message(number, mainOptions, body, footer, "sed1", messageId)
         replyReaction = replyReaction_Message(number, messageId, "🫡")
