9def916e95bb5e2f214def6a36c2b8c2ed02ee99
alalju <102770909+alalju@users.noreply.github.com>
2025-08-16 15:57:38 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask, request
-import sett 
+import sett
 import services
 
 app = Flask(__name__)
 
 @app.route('/bienvenido', methods=['GET'])
-def  bienvenido():
+def bienvenido():
     return 'Hola mundo bigdateros, desde Flask'
 
 @app.route('/webhook', methods=['GET'])
@@ -14,12 +15,12 @@ def verificar_token():
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
 
-        if token == sett.token and challenge != None:
+        if token == sett.token and challenge is not None:
             return challenge
         else:
             return 'token incorrecto', 403
     except Exception as e:
-        return e,403
+        return str(e), 403
     
 @app.route('/webhook', methods=['POST'])
 def recibir_mensajes():
@@ -35,11 +36,10 @@ def recibir_mensajes():
         name = contacts['profile']['name']
         text = services.obtener_Mensaje_whatsapp(message)
 
-        services.administrar_chatbot(text, number,messageId,name)
+        services.administrar_chatbot(text, number, messageId, name)
         return 'enviado'
 
     except Exception as e:
         return 'no enviado ' + str(e)
 
-if __name__ == '__main__':
-    app.run()
+
